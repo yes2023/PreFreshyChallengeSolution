49263a4e89d2e4c0c98731578d665f4c8d59bc5f
Pakkapong Attawiboon <pakkapong.att@mail.kmutt.ac.th>
2019-06-07 09:44:34 +0700

    Add Super GCD TestCase And solution

---
+++ Medium/Super GCD/SuperGCD.py
@@ -0,0 +1,25 @@
+import random
+import math
+
+def gcd(a, b):
+  while b:
+    a, b = b, a % b
+  return a
+
+inp = open("4.in", "w")
+ans = open("4.sol", "w")
+
+randed = random.randint(100000,100000000)
+
+multi = random.randint(0,math.floor(100000000/randed))
+num1 = multi*randed
+inp.write(str(num1) + "\n")
+
+multi = random.randint(0,math.floor(100000000/randed))
+num2 = multi*randed
+inp.write(str(num2))
+
+ans.write(str(gcd(num1,num2)))
+
+ans.close()
+inp.close()
